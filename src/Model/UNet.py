93d817547710097f0ad8cf6c9ebf31cf6e3055e0
zhengshizhao <shizhao.zheng@mail.utoronto.ca>
2025-03-23 22:34:06 -0400

    Fix some bugs

--- src/Model/UNet.py
+++ src/Model/UNet.py
@@ -10,13 +10,13 @@ class Block(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,padding=0, stride=1),
+            nn.Conv2d(in_channels, 2 * out_channels, kernel_size=kernel_size,padding=0, stride=1),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(inplace=True),
-            nn.Conv2d(out_channels, 2* out_channels, kernel_size=kernel_size,padding=0, stride=1),
+            nn.Conv2d(2 * out_channels, out_channels, kernel_size=kernel_size,padding=0, stride=1),
             nn.BatchNorm2d(2 * out_channels),
             nn.LeakyReLU(inplace=True),
-            nn.Conv2d(2 * out_channels, out_channels, kernel_size=kernel_size,padding=0, stride=1)
+            # nn.Conv2d(2 * out_channels, out_channels, kernel_size=kernel_size,padding=0, stride=1)
             # nn.BatchNorm2d(out_channels),
             # nn.ReLU(inplace=True)
         )
