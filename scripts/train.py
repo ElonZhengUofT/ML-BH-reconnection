8440b1056cbfb7e861be832a23fb32ecf8340ea2
zheng660 <shizhao.zheng@mail.utoronto.ca>
2025-05-01 12:38:03 -0400

    debug for train.py

--- scripts/train.py
+++ scripts/train.py
@@ -315,6 +315,7 @@ if __name__ == '__main__':
     # Choose the model based on the argument
 ################################################################################
     en_ch = len(feature_list) # 8 for 8 features, 9 for 9 features
+    print(en_ch)
     unet = ViTUNet(
         down_chs=(8, 64, 128),
         up_chs=(128, 64),
